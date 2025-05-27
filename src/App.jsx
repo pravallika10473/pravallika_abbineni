2224474578664b51d08b81618e36749946531579
Pravallika Abbineni <pravallikaabbineni@Pravallikas-MacBook-Pro.local>
2025-05-26 21:59:29 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="top-right-name">Pravallika Abbineni</div>
+      <div className="top-right-location">📍 Salt Lake City, Utah</div>
+      
+      <div className="body-text">
+        <p>I am continually in awe of our universe.</p>
+        <p>I studied artificial intelligence in college, now building <a href="https://www.spot-reader.com/">Spot</a> an Ai powered social e-reader platform. Learning something new every day and writing occasionally about what I find interesting.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
