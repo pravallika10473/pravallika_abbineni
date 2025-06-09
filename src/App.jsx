769a7be7427dd8ff53b5798362c4902f039a9937
Pravallika Abbineni <pravallikaabbineni@gmail.com>
2025-06-09 15:47:59 -0600

    twitter

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,16 @@ function App() {
 
   return (
     <>
-      <div className="top-right-name">Pravallika Abbineni</div>
+      <div className="top-right-name">
+        Pravallika Abbineni
+        <a href="https://x.com/pravallikaabbi3?s=21&t=DAmj8HyW6UwxveGc3QVd3A" target="_blank" rel="noopener noreferrer" className="x-logo">
+          <svg viewBox="0 0 24 24" aria-hidden="true" className="x-icon">
+            <g>
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
+            </g>
+          </svg>
+        </a>
+      </div>
       <div className="top-right-location">Salt Lake City, Utah</div>
       
       <div className="body-text">
