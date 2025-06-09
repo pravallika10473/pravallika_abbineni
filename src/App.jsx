5db8cc329b2023ee87c411719ff620aa69fd58eb
Pravallika Abbineni <pravallikaabbineni@gmail.com>
2025-06-09 10:42:26 -0600

    blog

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,16 @@ function App() {
       
       <div className="body-text">
         <p>Constantly in awe of human collective knowledge.</p>
-        <p>I studied artificial intelligence in college, now building <a href="https://www.spot-reader.com/">Spot</a> an Ai powered social e-reader platform. I write occasionally about things I find interesting.</p>
+        <p>I studied artificial intelligence in college and now I’m building <a href="https://www.spot-reader.com/">Spot</a>, a full-stack learning platform that leverages the collective intelligence of humans and AI to help support deep, self-directed learning. I write occasionally about things I find interesting.</p>
+        <br />
+      </div>
+
+      <div className="header-text">
+        <p>Ideas in Progress:</p>
+        <ul>
+          <li><a href="https://medium.com/@pravallikaabbineni/how-to-master-anything-68cc58ba30df">How to Learn Anything</a></li>
+          <li><a href="https://medium.com/@pravallikaabbineni/life-58e7e19e0001">Genome</a></li>
+        </ul>
       </div>
     </>
   )
